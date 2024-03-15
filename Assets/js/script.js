9a96d3547a8aa87511d3201eda5c8d0efd40e9bf
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-15 20:08:45 +0100

    Add: html elements

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,24 @@
-let number = 0;
-console.log(number);
+// 1 - L'utente clicca su un bottone che genererà una griglia di 
+//     gioco quadrata.
+
+// 2 - Ogni cella ha un numero progressivo, da 1 a 100.
+//     ci saranno quindi 10 caselle per ognuna delle 10 righe.
+
+// 3 - Quando l'utente clicca su ogni cella, 
+
+// 4 - la cella cliccata si colora di azzurro 
+
+// 5 - ed emetto un messaggio in console 
+//     con il numero della cella cliccata.
+
+
+//Consigli del giorno:  :party_wizard:
+//Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+//Ad esempio:
+//Di cosa ho bisogno per generare i numeri?
+//Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+
+
+//START
+
+// 1 - creare un bottone che genererà la griglia
