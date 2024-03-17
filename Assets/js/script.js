6854e9bde783e1a3e96ebf6f8e571c0ca4b63a93
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-17 22:51:07 +0100

    Fixed: modified entire script to add bonus (mode easy - medium - hard) - need reset

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,10 @@
+// Bonus
+//Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+
+//- con difficoltà 1 => 100 caselle, 
+//- con difficoltà 2 => 81 caselle
+//- con difficoltà 3 => 49 caselle
+
 // 1) - L'utente clicca su un bottone che genererà una griglia di 
 //     gioco quadrata.
 
@@ -16,77 +23,49 @@
 // 1) - CREARE UN BOTTONE CHE GENERERA' LA GRIGLIA:
 //      creo un evento click in cui rimuovo la classe d-none.
 
-//creo costante per selezione della griglia
+//creo selettore per la griglia
 const gridGame = document.querySelector("#grid-game");
 
-//seleziono tasto play e gli aggiungo evento click
-const buttonPlay = document.querySelector(".button-play");
-buttonPlay.addEventListener ('click', function () {
-    //al click togliamo alla griglia la class d-none per farla apparire.
-    gridGame.classList.toggle('d-none');
+//creo selettore per il tasto play 
+const buttonPlay = document.querySelector("#button-play");
+
+
+
+//creo evento al play che fa comparire la griglia
+buttonPlay.addEventListener ('click', function (){ 
+//creo selettore difficoltà selezionata.
+const choice = document.querySelector("#levels").value;
+    gridGame.classList.remove('d-none');
+    
+    let numbersOfSquare;
+// Creazione numero dei quadrati in base scelta selezionata.
+if (choice === 'easy') {
+    numbersOfSquare = 100;
+} else if (choice === 'medium') {
+    numbersOfSquare = 81;
+} else if (choice === 'hard') {
+    numbersOfSquare = 49;
+}
+
+// la griglia compare al click, ora mi servono i quadrati.
+// ho a disposizione il numero degli elementi al click in base alla difficoltà.
+// devo creare una funzione che crea un numero di div pari a numbersOfsquare.
+
+  // Ciclo for che genera un div tante volte quanto il numero degli squares.
+  for (let i = 1; i <= numbersOfSquare; i++) {
+    const newSquare = createDiv(i);
+    gridGame.append(newSquare);
+}
 });
 
-
-// 2) - OGNI CELLA HA UN NUMERO PROGRESSIVO da 1 a 100.
-//     CI SARANNO 10 CELLE PER OGNUNA DELLE 100 RIGHE:
-    
-    //array vuoto
-    const blackList = [];
-
-    // il ciclo genera numeri da 1 a 100
-    for(let i = 0; i < 100; i++) {
-         
-        //costante con funzione di generazione numeri random.
-        const randomNumber = getRandomUniqueNumber(1, 100, blackList);
-        
-        //pusho numeri(1-100) in array
-        blackList.push(randomNumber);
-        
-        //creo costante square con funzione di generazione square in html,
-        const square = generateSquare(randomNumber);     
-       
-        //appendo quadrati alla griglia
-        gridGame.append(square);
-        
-// 3) - QUANDO L'UTENTE CLICCA SU OGNI CELLA,
-//     LA CELLA CLICCATA SI COLORA DI AZZURRO:
-        square.addEventListener('click', function () {       
-            square.classList.add('styles-selection'); 
-            console.log(randomNumber);  
-        });
-        };
-        
-    // ------- FUNCTIONS ------- //
-    
-    // Funzione per generazione quadrato con all'interno uno span che a sua volta
-    // conterrà indice creato in ciclo for.
-    function generateSquare(number) {
-        const newSquare = document.createElement('div');
-        newSquare.classList.add('square');
-        newSquare.innerHTML = `<span>${number}</span>`;
-        return newSquare;
-    }
-    
-    // Generazione quadrato con all'interno uno span che a sua volta
-    // conterrà indice creato in ciclo for.
-    function getRandomUniqueNumber(min, max, blackList) { 
-        let numbIsValid = false;
-        let randomNumber;
-        while(!numbIsValid) {
-            randomNumber = getRndInteger(min, max);
-            if(!blackList.includes(randomNumber)) {
-                numbIsValid = true;
-            }
-        }  
-        return randomNumber;
-    }
-    
-    //Funzione per generare un numero casuale intero compreso tra min e max.
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
-
-    
+// FUNCTIONS
+// Funzione che genera un div
+   function createDiv (number) {
+   let newDiv = document.createElement('div');
+   newDiv.classList.add('square');
+   newDiv.textContent = number; // Aggiungo il numero al contenuto del div
+   return newDiv; 
+}
 
 
-    
+
